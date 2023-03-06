4804586b46f0d7156c5eaa16fe1982a9bbe87edf
lincoln gibson <lincolngibson7@gmail.com>
2023-03-05 21:17:59 -0300

    final update

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// index.js
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
- * DATA is the object that contains all
- * the data to be provided to Mustache
- * Notice the "name" and "date" property.
- */
+
 let DATA = {
-	name: 'Lincoln',
-	date: new Date().toLocaleDateString('en-US', {
+	refresh_date: new Date().toLocaleDateString('en-US', {
 		weekday: 'long',
 		month: 'long',
 		day: 'numeric',
@@ -19,11 +13,7 @@ let DATA = {
 		timeZone: 'America/Sao_Paulo',
 	}),
 };
-/**
- * A - We open 'main.mustache'
- * B - We ask Mustache to render our file with the data
- * C - We create a README.md file with the generated output
- */
+
 function generateReadMe() {
 	fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
 		if (err) throw err;
